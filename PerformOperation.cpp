83a871fdd197444b23d8dd66e6ea3abaf3d49080
Sophia Leeseberg <101421081+M1suaki@users.noreply.github.com>
2024-11-23 14:00:13 -0600

    Update PerformOperation.cpp

--- PerformOperation.cpp
+++ PerformOperation.cpp
@@ -1,4 +1,6 @@
-#include <iostream> using namespace std;
+#include <iostream>
+using namespace std;
+
 int PerformOperation(int operand1, char oper, int operand2){ 
   if(oper== '+'){
 return operand1 + operand2;
@@ -20,6 +22,7 @@ cout <<"Error\n";
 return 0;
 }
 }
+
 int main(){
 cout << PerformOperation(1, '+', 2) << endl; 
 cout << PerformOperation(3,'-',1) << endl; 
